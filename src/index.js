c5e2cf867ea35197bf513c67e0dedf6e8bdfc222
Olesya <krayanskaya_ov@mail.ru>
2023-11-18 22:30:01 +0300

    validate

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const form = document.querySelector('.form-auth');
+const inputs = form.querySelectorAll('.form-auth__input_validate');
+const checkboxRemember = document.getElementById('remember');
+const sendForm = document.getElementById('send');
+
+inputs.forEach((input) => {
+  input.addEventListener('blur', () => {
+    input.setAttribute('required', '');
+  });
+});
+
+sendForm.addEventListener('mousedown', () => {
+  inputs.forEach((input) => {
+    input.setAttribute('required', '');
+  });
+});
